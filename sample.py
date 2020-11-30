5d6aae6c984812d05f068b16ab84fbf20d5c5651
Akihiro Yamashita <yamashita@tokyo-ct.ac.jp>
2020-12-01 01:40:06 +0900

    update

--- sample.py
+++ sample.py
@@ -8,5 +8,5 @@ temp = BME280.readTemp()
 humi = BME280.readHumi()
 
 print "pressure : %7.2f hPa" % pres
-print "temperature : %-6.2f ℃" % temp
-print "humidity : %6.2f ％" % humi
+print "temperature : %-6.2f C" % temp
+print "humidity : %6.2f %%" % humi
